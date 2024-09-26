412c1fd34c6bbb3e3509b20145ce917137d06d5d
tsukumi <tsukumijima@users.noreply.github.com>
2024-09-27 00:47:43 +0900

    Update: 音声合成モデル管理画面の UI の実装を進める

--- src/components/Dialog/ModelManageDialog.vue
+++ src/components/Dialog/ModelManageDialog.vue
@@ -18,29 +18,21 @@
             <QList class="model-list">
               <QItem v-for="aivmInfo in Object.values(aivmInfoDict)" :key="aivmInfo.manifest.uuid" v-ripple class="q-pr-none" clickable
                 :active="activeAivmUuid === aivmInfo.manifest.uuid" @click="activeAivmUuid = aivmInfo.manifest.uuid">
-                  <QItemSection avatar>
-                    <QAvatar rounded color="primary">
-                      <img :src="aivmInfo.manifest.speakers[0].styles[0].icon" />
-                    </QAvatar>
-                  </QItemSection>
-                  <QItemSection>
-                    <QItemLabel class="text-display">{{ aivmInfo.manifest.name }}</QItemLabel>
-                    <QItemLabel caption class="engine-path">
-                      {{ aivmInfo.manifest.speakers.length }} Speakers / Version {{ aivmInfo.manifest.version }}
-                    </QItemLabel>
+                <QItemSection avatar>
+                  <QAvatar rounded color="primary">
+                    <img :src="aivmInfo.manifest.speakers[0].styles[0].icon" />
+                  </QAvatar>
+                </QItemSection>
+                <QItemSection>
+                  <QItemLabel class="text-display">{{ aivmInfo.manifest.name }}</QItemLabel>
+                  <QItemLabel caption class="engine-path">
+                    {{ aivmInfo.manifest.speakers.length }} Speakers / Version {{ aivmInfo.manifest.version }}
+                  </QItemLabel>
                 </QItemSection>
               </QItem>
             </QList>
           </div>
           <div v-if="activeAivmInfo" class="model-detail" style="width: 100%;">
-            <div class="q-pa-md">
-              <div style="font-size: 20px; font-weight: bold;">
-                {{ activeAivmInfo.manifest.name }}
-              </div>
-              <div class="q-mt-sm" style="font-size: 13.5px; color: #D2D3D4;">
-                {{ activeAivmInfo.manifest.description === '' ? '（この音声合成モデルの説明は提供されていません）' : activeAivmInfo.manifest.description }}
-              </div>
-            </div>
             <!-- タブは複数の話者がモデルに含まれる場合のみ表示する -->
             <QTabs v-if="activeAivmInfo && activeAivmInfo.manifest.speakers.length > 1" v-model="activeSpeakerIndex"
               dense activeColor="primary">
@@ -51,6 +43,34 @@
             <QTabPanels v-model="activeSpeakerIndex"
               animated class="bg-background">
               <QTabPanel v-for="(speaker, index) of activeAivmInfo.manifest.speakers" :key="speaker.uuid" :name="index">
+                <div class="q-mt-sm row items-center">
+                  <div class="col-auto" style="font-size: 20px; font-weight: bold;">
+                    <span>{{ activeAivmInfo.manifest.name }}</span>
+                    <!-- 音声合成モデル名と現在の話者名が異なる場合のみ、話者名を追加する -->
+                    <span v-if="activeAivmInfo.manifest.name !== speaker.name">
+                      - {{ speaker.name }}
+                    </span>
+                  </div>
+                  <div class="col-auto q-ml-auto" style="font-size: 13.5px; color: #D2D3D4;">
+                    <span>{{ activeAivmInfo.manifest.speakers.length }} Speakers / Version {{ activeAivmInfo.manifest.version }}</span>
+                    <!-- 音声合成モデルのバージョンと現在の話者のバージョンが一致しない場合のみ、Speaker Version を表示する -->
+                    <span v-if="activeAivmInfo.manifest.version !== activeAivmInfo.manifest.speakers[activeSpeakerIndex].version">
+                      (Speaker Version {{ activeAivmInfo.manifest.speakers[activeSpeakerIndex].version }})
+                    </span>
+                  </div>
+                </div>
+                <div class="q-mt-sm row items-center">
+                  <div class="col-auto" style="font-size: 15px; font-weight: bold;">
+                    {{ activeAivmInfo.manifest.speakers.reduce((acc, speaker) => acc + speaker.styles.length, 0) }}スタイル
+                  </div>
+                  <div class="col-auto q-ml-sm" style="font-size: 13.5px; font-weight: bold; color: #D2D3D4;">
+                    {{ activeAivmInfo.manifest.speakers[activeSpeakerIndex].styles.map(style => style.name).join(' / ') }}
+                  </div>
+                </div>
+                <div class="q-mt-md" style="font-size: 13.5px; color: #D2D3D4;">
+                  {{ activeAivmInfo.manifest.description === '' ? '（この音声合成モデルの説明は提供されていません）' : activeAivmInfo.manifest.description }}
+                </div>
+                <div class="q-mt-md" style="font-size: 17px; font-weight: bold;">ボイスサンプル</div>
               </QTabPanel>
             </QTabPanels>
           </div>
